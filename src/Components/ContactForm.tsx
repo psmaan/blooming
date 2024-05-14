dc523ff05b2397d2f2f8c881e0ad808c681a9714
AzmeerHamasAli <apnawaqtaiga@gmail.com>
2024-05-14 14:45:30 +0500

    123

--- src/Components/ContactForm.tsx
+++ src/Components/ContactForm.tsx
@@ -19,48 +19,7 @@ const ContactForm: React.FC = () => {
             id="standard-basic"
             label="What's Your Name?"
             variant="standard"
-            sx={{
-              color: "red",
-              "& .MuiInputLabel-root": {
-                color: "white",
-                fontSize: "1.5rem",
-                fontFamily: "Recoleta",
-              },
-              "& .MuiInput-root": {
-                fontFamily: "Recoleta",
-              },
-              "& .css-1eed5fa-MuiInputBase-root-MuiInput-root::after": {
-                borderBottom: "none",
-                transition: "all",
-                position: "absolute",
-                bottom: 0,
-                left: 0,
-                width: "25%",
-                height: "2px",
-                backgroundColor: "#B2FF02",
-                color: "#B2FF02",
-              },
-              "& .css-1eed5fa-MuiInputBase-root-MuiInput-root::before": {
-                borderBottom: "none",
-              },
-
-              "& .css-1eed5fa-MuiInputBase-root-MuiInput-root": {
-                position: "relative",
-
-                marginTop: "40px",
-                borderBottom: "1px solid #B2FF02",
-              },
-              "& .css-aqpgxn-MuiFormLabel-root-MuiInputLabel-root": {
-                color: "white",
-              },
-              "& .css-1c2i806-MuiFormLabel-root-MuiInputLabel-root.Mui-focused":
-                {
-                  color: "white",
-                },
-              "& .css-1x51dt5-MuiInputBase-input-MuiInput-input": {
-                color: "white",
-              },
-            }}
+            className="inputArea"
           />
         </div>
         <div className="flex flex-col gap-1">
@@ -68,48 +27,7 @@ const ContactForm: React.FC = () => {
             id="standard-basic"
             label="What's Your Email?"
             variant="standard"
-            sx={{
-              color: "red",
-              "& .MuiInputLabel-root": {
-                color: "white",
-                fontSize: "1.5rem",
-                fontFamily: "Recoleta",
-              },
-              "& .MuiInput-root": {
-                fontFamily: "Recoleta",
-              },
-              "& .css-1eed5fa-MuiInputBase-root-MuiInput-root::after": {
-                borderBottom: "none",
-                transition: "all",
-                position: "absolute",
-                bottom: 0,
-                left: 0,
-                width: "25%",
-                height: "2px",
-                backgroundColor: "#B2FF02",
-                color: "#B2FF02",
-              },
-              "& .css-1eed5fa-MuiInputBase-root-MuiInput-root::before": {
-                borderBottom: "none",
-              },
-
-              "& .css-1eed5fa-MuiInputBase-root-MuiInput-root": {
-                position: "relative",
-
-                marginTop: "40px",
-                borderBottom: "1px solid #B2FF02",
-              },
-              "& .css-aqpgxn-MuiFormLabel-root-MuiInputLabel-root": {
-                color: "white",
-              },
-              "& .css-1c2i806-MuiFormLabel-root-MuiInputLabel-root.Mui-focused":
-                {
-                  color: "white",
-                },
-              "& .css-1x51dt5-MuiInputBase-input-MuiInput-input": {
-                color: "white",
-              },
-            }}
+            className="inputArea"
           />
         </div>
         <div className="flex flex-col gap-1">
@@ -120,49 +38,7 @@ const ContactForm: React.FC = () => {
             variant="standard"
             multiline
             rows={10}
-            sx={{
-              color: "red",
-              "& .MuiInputLabel-root": {
-                color: "white",
-                fontSize: "1.5rem",
-                fontFamily: "Recoleta",
-              },
-              "& .MuiInput-root": {
-                fontFamily: "Recoleta",
-              },
-              "& .css-uhyr2s-MuiInputBase-root-MuiInput-root::after": {
-                borderBottom: "none",
-                transition: "all",
-                position: "absolute",
-                bottom: 0,
-                left: 0,
-                width: "25%",
-                height: "2px",
-                backgroundColor: "#B2FF02",
-                color: "#B2FF02",
-              },
-              "& .css-uhyr2s-MuiInputBase-root-MuiInput-root::before": {
-                borderBottom: "none",
-              },
-
-              "& .css-uhyr2s-MuiInputBase-root-MuiInput-root": {
-                position: "relative",
-                color: "white",
-
-                marginTop: "40px",
-                borderBottom: "1px solid #B2FF02",
-              },
-              "& .css-aqpgxn-MuiFormLabel-root-MuiInputLabel-root": {
-                color: "white",
-              },
-              "& .css-1c2i806-MuiFormLabel-root-MuiInputLabel-root.Mui-focused":
-                {
-                  color: "white",
-                },
-              "& .css-1x51dt5-MuiInputBase-input-MuiInput-input": {
-                color: "white",
-              },
-            }}
+            className="textArea"
           />
         </div>
 
